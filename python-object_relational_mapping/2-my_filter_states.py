b9ee738f9020432fbbd7b3d9b209181c1063ee13
Adamzou-lab <adamzpro@icloud.com>
2026-06-09 10:53:14 +0200

    fix: modify SQL query to use BINARY for case-sensitive state filtering

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute(
         "SELECT * FROM states "
-        "WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        "WHERE BINARY name = '{}' ORDER BY id ASC".format(state_name)
     )
     rows = cur.fetchall()
     for row in rows:
